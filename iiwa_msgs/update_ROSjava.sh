f62b147def3abe6ba7158b43d551bd56abfa6bd6
peijiexu <peijiexu99@gmail.com>
2022-05-14 15:29:50 -0700

    try new jar files

---
+++ iiwa_msgs/update_ROSjava.sh
@@ -0,0 +1,6 @@
+# copy the new jar files to iiwa_ros_java
+
+cp rosjava_messages/actionlib_msgs/1.12.7/actionlib_msgs-1.12.7.jar ../iiwa_ros_java/ROSJavaLib
+cp rosjava_messages/geometry_msgs/1.12.7/geometry_msgs-1.12.7.jar ../iiwa_ros_java/ROSJavaLib
+cp rosjava_messages/iiwa_msgs/2.3.1/iiwa_msgs-2.3.1.jar ../iiwa_ros_java/ROSJavaLib
+cp rosjava_messages/std_msgs/0.5.11/std_msgs-0.5.11.jar ../iiwa_ros_java/ROSJavaLib
